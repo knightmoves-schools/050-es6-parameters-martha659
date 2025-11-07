390d3c213fdc9b9e575f99354d55999f0c4cf587
Martha659 <martha.blackcloud@students.msswarriors.org>
2025-11-07 19:45:54 +0000

    1

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 
+function calcuateTotal(subtotal, shipping = ) => {
+    return subtotal + shipping;
+}
+
+calcuateTotal result = shipping (2.50);
+
+function printTopThreeHeadlines = (...headlines) => {
+    return headlines[0] + headlines[1] + headlines[3]
+};
+
+result = printTopThreeHeadlines(result 1, 2, 3, 4, 5,);
+
+
+
+
